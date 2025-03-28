37d067e8a01e21b0e5718ecad6a609cdac038977
Abideen926 <muhammad.abideen@kamsoft.co>
2025-03-28 20:46:00 +0500

    changed domain name

--- next.config.mjs
+++ next.config.mjs
@@ -6,28 +6,69 @@ const nextConfig = {
   },
   transpilePackages: ["mui-tel-input"],
   images: {
-    domains: ["10.10.1.2"],
+    domains: ["https://ralli.logodesignagency.co/"],
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "10.10.1.2",
+        hostname: "ralli.logodesignagency.co",
         port: "8000",
         pathname: "/assets/images/profiles/**",
       },
       {
         protocol: "http",
-        hostname: "10.10.1.2",
+        hostname: "ralli.logodesignagency.co",
         port: "8000",
         pathname: "/assets/posts/media/**",
       },
       {
         protocol: "http",
-        hostname: "10.10.1.2",
+        hostname: "ralli.logodesignagency.co",
+        port: "8000",
+        pathname: "/storage/assets/posts/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "ralli.logodesignagency.co",
+        port: "8000",
+        pathname: "/storage/assets/posts/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ralli.logodesignagency.co",
         port: "8000",
         pathname: "/storage/assets/posts/media/**",
       },
     ],
   },
+  // images: {
+  //   domains: ["10.10.1.2"],
+  //   remotePatterns: [
+  //     {
+  //       protocol: "http",
+  //       hostname: "10.10.1.2",
+  //       port: "8000",
+  //       pathname: "/assets/images/profiles/**",
+  //     },
+  //     {
+  //       protocol: "http",
+  //       hostname: "10.10.1.2",
+  //       port: "8000",
+  //       pathname: "/assets/posts/media/**",
+  //     },
+  //     {
+  //       protocol: "http",
+  //       hostname: "10.10.1.2",
+  //       port: "8000",
+  //       pathname: "/storage/assets/posts/media/**",
+  //     },
+  //     {
+  //       protocol: "http",
+  //       hostname: "10.10.1.2",
+  //       port: "8000",
+  //       pathname: "/storage/assets/posts/media/**",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
