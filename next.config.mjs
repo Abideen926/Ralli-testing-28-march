b0a85ce6902567849397cd3f5f10d5216e61623a
Abideen926 <muhammad.abideen@kamsoft.co>
2025-03-28 20:50:36 +0500

    changed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.cache = false;
+//     return config;
+//   },
+//   transpilePackages: ["mui-tel-input"],
+//   images: {
+//     domains: ["10.10.1.2"],
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "10.10.1.2",
+//         port: "8000",
+//         pathname: "/assets/images/profiles/**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "10.10.1.2",
+//         port: "8000",
+//         pathname: "/assets/posts/media/**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "10.10.1.2",
+//         port: "8000",
+//         pathname: "/storage/assets/posts/media/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
@@ -6,69 +41,27 @@ const nextConfig = {
   },
   transpilePackages: ["mui-tel-input"],
   images: {
-    domains: ["https://ralli.logodesignagency.co/"],
+    domains: ["ralli.logodesignagency.co", "10.10.1.2"],
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "ralli.logodesignagency.co",
+        hostname: "10.10.1.2",
         port: "8000",
         pathname: "/assets/images/profiles/**",
       },
       {
         protocol: "http",
-        hostname: "ralli.logodesignagency.co",
+        hostname: "10.10.1.2",
         port: "8000",
         pathname: "/assets/posts/media/**",
       },
       {
-        protocol: "http",
-        hostname: "ralli.logodesignagency.co",
-        port: "8000",
-        pathname: "/storage/assets/posts/media/**",
-      },
-      {
-        protocol: "http",
-        hostname: "ralli.logodesignagency.co",
-        port: "8000",
-        pathname: "/storage/assets/posts/media/**",
-      },
-      {
         protocol: "https",
         hostname: "ralli.logodesignagency.co",
-        port: "8000",
         pathname: "/storage/assets/posts/media/**",
       },
     ],
   },
-  // images: {
-  //   domains: ["10.10.1.2"],
-  //   remotePatterns: [
-  //     {
-  //       protocol: "http",
-  //       hostname: "10.10.1.2",
-  //       port: "8000",
-  //       pathname: "/assets/images/profiles/**",
-  //     },
-  //     {
-  //       protocol: "http",
-  //       hostname: "10.10.1.2",
-  //       port: "8000",
-  //       pathname: "/assets/posts/media/**",
-  //     },
-  //     {
-  //       protocol: "http",
-  //       hostname: "10.10.1.2",
-  //       port: "8000",
-  //       pathname: "/storage/assets/posts/media/**",
-  //     },
-  //     {
-  //       protocol: "http",
-  //       hostname: "10.10.1.2",
-  //       port: "8000",
-  //       pathname: "/storage/assets/posts/media/**",
-  //     },
-  //   ],
-  // },
 };
 
 export default nextConfig;
